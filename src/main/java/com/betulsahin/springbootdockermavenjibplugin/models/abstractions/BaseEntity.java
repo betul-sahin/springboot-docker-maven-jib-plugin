ff6afa2f3416796c3c681b4dafb7f2cdaef42ff8
Betül Şahin <betulsahin3387@hotmail.com>
2021-10-25 18:46:06 +0300

    Add Product.java and BaseEntity.java to models layer

---
+++ src/main/java/com/betulsahin/springbootdockermavenjibplugin/models/abstractions/BaseEntity.java
@@ -0,0 +1,70 @@
+package com.betulsahin.springbootdockermavenjibplugin.models.abstractions;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.Instant;
+import java.util.Objects;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @JsonIgnore
+    @CreatedDate
+    private Instant createdDate = Instant.now();
+
+    @JsonIgnore
+    @LastModifiedDate
+    private Instant lastModifiedDate = Instant.now();
+
+    public BaseEntity() {
+    }
+
+    public BaseEntity(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Instant getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Instant createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Instant getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Instant lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof BaseEntity)) return false;
+        BaseEntity that = (BaseEntity) o;
+        return getId() == that.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
