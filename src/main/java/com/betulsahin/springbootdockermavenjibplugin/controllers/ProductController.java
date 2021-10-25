a16033db2895c5a30b4f8430dbb028a852e021ef
Betül Şahin <betulsahin3387@hotmail.com>
2021-10-25 22:35:48 +0300

    Add ProductController.java to controllers layer

---
+++ src/main/java/com/betulsahin/springbootdockermavenjibplugin/controllers/ProductController.java
@@ -0,0 +1,38 @@
+package com.betulsahin.springbootdockermavenjibplugin.controllers;
+
+import com.betulsahin.springbootdockermavenjibplugin.dto.ProductDto;
+import com.betulsahin.springbootdockermavenjibplugin.models.Product;
+import com.betulsahin.springbootdockermavenjibplugin.services.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/v1/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<Product> create(@Valid @RequestBody ProductDto request){
+
+        Optional<Product> productOptional = productService.create(request);
+        if(productOptional.isPresent()){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(productOptional.get(), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductDto>> getAll(){
+        return new ResponseEntity<>(productService.getAll(), HttpStatus.OK);
+    }
+
+}
