02e9442930f416d3f2b5889aa1034a41db0e3eb2
Betül Şahin <betulsahin3387@hotmail.com>
2021-10-25 12:08:24 +0300

    Create Spring Boot project and add dependencies

---
+++ src/main/java/com/betulsahin/springbootdockermavenjibplugin/SpringbootDockerMavenJibPluginApplication.java
@@ -0,0 +1,13 @@
+package com.betulsahin.springbootdockermavenjibplugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDockerMavenJibPluginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDockerMavenJibPluginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/betulsahin/springbootdockermavenjibplugin/SpringbootDockerMavenJibPluginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betulsahin.springbootdockermavenjibplugin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDockerMavenJibPluginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
